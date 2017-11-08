6e4a5acc1d038e578cfde14ae21573e3e5f75ad2
jpleva91 <jpleva91@gmail.com>
2017-11-08 11:36:35 -0700

    updating

---
+++ back-end/models/index.js
@@ -0,0 +1,1 @@
+let sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://Prime@localhost:5432/dogs_for_days');
--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,29 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', function(req, res) {
-	res.send('<h1>Fancy seeing you here.</h1>');
+//CORS setup to allow other ports from this host
+
+//Only needed if not on Heroku/prod
+if(!process.env.DYNO) {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    next();
+  });
+}
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(3000, function() {
-	console.log('listening on port 3000');
+let port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
